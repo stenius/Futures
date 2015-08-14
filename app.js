93eed6c7a20a7f08dc6091f0ecd9b4af4f46561f
Paul Stenius <stenius@gmail.com>
2015-08-14 17:09:48 -0500

    add pouchdb and stub out adding events to cal

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var $ = require('jquery'),
 	fs = require('moment'),
-	ipc = require('fullcalendar')
+	ipc = require('fullcalendar'),
+	PouchDB = require('pouchdb')
 
 $(document).ready(function() {
 	// page is now ready, initialize the calendar...
 	$('#calendar').fullCalendar({
 		// put your options and callbacks here
-	})
+		events: function(start, end, timezone, callback) {
+			console.log(start);
+			console.log(end);
+			console.log(timezone);
+			var events = [
+				{
+					title  : 'event1',
+					start  : '2015-08-01'
+				},
+				{
+					title  : 'event2',
+					start  : '2015-08-05',
+					end    : '2015-08-07'
+				}
+			];
+			callback(events);
+		}
+		});
 });
+
+var db = new PouchDB('transactions', {adapter: 'websql'});
