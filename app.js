b2b1df291c9acd10a257519111ecc1728ce6586d
Paul Stenius <stenius@gmail.com>
2015-08-13 22:28:18 -0500

    update project to use calendar

---
+++ app.js
@@ -0,0 +1,10 @@
+var $ = require('jquery'),
+	fs = require('moment'),
+	ipc = require('fullcalendar')
+
+$(document).ready(function() {
+	// page is now ready, initialize the calendar...
+	$('#calendar').fullCalendar({
+		// put your options and callbacks here
+	})
+});
