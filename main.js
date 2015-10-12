52faf7f7267bc5f16cbeb20a4e1d1d996341c440
Paul Stenius <stenius@gmail.com>
2015-10-11 21:49:11 -0500

    build up a context menu for the tray

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var Tray = require('tray');  // Module to create native browser window.
+var Menu = require('menu');
 
 // Report crashes to our server.
 require('crash-reporter').start();
@@ -8,6 +9,7 @@ require('crash-reporter').start();
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
+var appIcon = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -18,14 +20,64 @@ app.on('window-all-closed', function() {
   }
 });
 
+var logToBrowser = function() {
+if (mainWindow && mainWindow !== null) {
+var baseArgs = ['console.log']
+mainWindow.webContents.send.apply(mainWindow.webContents, baseArgs.concat([].slice.call(arguments)))
+}
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 900, icon: 'stats100.png'});
-  //mainWindow = new BrowserWindow({width: 570, height: 477, icon: 'stats100.png'});
-  var appIcon = new Tray('stats100.png');
+  //mainWindow = new BrowserWindow({width: 800, height: 900, icon: 'stats100.png'});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    // height: 650,
+    height: 900,
+    icon: 'stats100.png',
+	center: true,
+	'skip-taskbar': true,
+	frame: false,
+	show: false
+  });
+  appIcon = new Tray('stats100.png');
   appIcon.setTitle('Futures');
+  //var contextMenu = new Menu();
+  //contextMenu.append(new MenuItem({ label: 'MenuItem1', click: function() { console.log('item 1 clicked'); } }));
+
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Show/Hide Venmo',
+      //type: 'radio',
+      click: function(){
+		if (mainWindow.isVisible()) {
+		  mainWindow.hide()
+		} else {
+		  //mainWindow.setPosition(parseInt(bounds.x + (bounds.width / 2) - 400), bounds.y + bounds.height)
+		  mainWindow.show()
+		}
+      }
+    },
+  ]);
+  //appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
+
+  appIcon.on('clicked', function(e, bounds) {
+    logToBrowser('app icon clicked', e)
+    logToBrowser('app icon clicked', bounds)
+    console.log(bounds)
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.setPosition(parseInt(bounds.x + (bounds.width / 2) - 400), bounds.y + bounds.height)
+      mainWindow.show()
+    }
+  })
+
+
+
+
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
